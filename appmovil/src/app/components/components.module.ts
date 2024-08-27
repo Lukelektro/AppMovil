94325a535ba1b74a93006718e0e636f664654e2d
Martin Lopez <martinantulopez@gmail.com>
2024-08-27 15:42:09 -0400

    Vista login y modelo de usuario terminado

--- appmovil/src/app/components/components.module.ts
+++ appmovil/src/app/components/components.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    NavbarComponent
 
   ]
 })
--- appmovil/src/app/pages/login/login.page.ts
+++ appmovil/src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Usuario } from 'src/app/models/ususario.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  passwordType: string = 'password'
+  passwordIcon: string = 'eye-off'
+  usuario: Usuario = new Usuario();
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  MostrarPassword(): void{
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';
+      this.passwordIcon = 'eye';
+    } else {
+      this.passwordType = 'password';
+      this.passwordIcon = 'eye-off';
+    }
   }
 
 }
