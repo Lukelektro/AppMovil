a2938f875d4a7af29223d12eda6904fa5def1ef0
Luke-PC <luka.martinez@duocuc.cl>
2024-08-28 11:58:11 -0400

    agregada las cosas que faltaban para que funcionara

--- appmovil/src/app/pages/login/login.page.ts
+++ appmovil/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  usuario: Usuario = new Usuario();
+
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  MostrarPassword() {
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';
+      this.passwordIcon = 'eye';
+    } else {
+      this.passwordType = 'password';
+      this.passwordIcon = 'eye-off';
+    }
   }
 
 }
