45895e0bf6f108471b641723fb3444c5c530fc93
Luke-PC <luka.martinez@duocuc.cl>
2024-08-28 11:53:36 -0400

    agregado modo oscuro y claro, ruta de componentes y lo que hizo el martin (login)

--- appmovil/src/app/components/components.module.ts
+++ appmovil/src/app/components/components.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BottombarComponent } from './bottombar/bottombar.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    BottombarComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    NavbarComponent
-
+    NavbarComponent,
+    BottombarComponent
   ]
 })
 export class ComponentsModule {}
